fdfcb64e0088022d0a61a22d301fb4417fbf3b2c
Georgia Mills <georgee_123@hotmail.com>
2016-09-06 18:05:24 +0100

    first commit

---
+++ Flare/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flare
+//
+//  Created by Georgia Mills on 06/09/2016.
+//  Copyright © 2016 appflare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
