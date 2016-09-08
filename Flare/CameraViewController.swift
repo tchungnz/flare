a740455ff556a17440e3b41096a4c660df01f3fd
Tommy Williams <tom@teow.co.uk>
2016-09-08 17:01:03 +0100

    Full screen camera and retake button

--- Flare/CameraViewController.swift
+++ Flare/CameraViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
     
+    @IBOutlet weak var cameraView: UIView!
     
-    @IBOutlet weak var Camera: UIButton!
-    @IBOutlet weak var ImageDisplay: UIImageView!
+//    @IBOutlet weak var Camera: UIButton!
+//    @IBOutlet weak var ImageDisplay: UIImageView!
     
     
     override func viewDidLoad() {
@@ -27,22 +32,109 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        var backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        var error : NSError?
+        var input = AVCaptureDeviceInput()
+        do {
+         input = try AVCaptureDeviceInput(device: backCamera)
+        } catch {
+            error
+        }
+        
+        if error == nil && (captureSession?.canAddInput(input))! {
+            
+            captureSession?.addInput(input)
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if ((captureSession?.canAddOutput(stillImageOutput)) != nil) {
+                
+                captureSession?.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession?.startRunning()
+                
+            }
+        }
+    }
+    
+    
 
     
-    @IBAction func CameraAction(sender: UIButton) {
+//    @IBAction func CameraAction(sender: UIButton) {
+//        
+//        let picker = UIImagePickerController()
+//        picker.delegate = self
+//        
+//        
+//        picker.sourceType = .Camera
+//        
+//        presentViewController(picker, animated: true, completion: nil)
+//        
+//    }
+//    
+//    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+//        ImageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
+//    }
+    @IBOutlet weak var tempImageView: UIImageView!
+    func didPressTakePhoto() {
         
-        let picker = UIImagePickerController()
-        picker.delegate = self
+        if let videoConnection = stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo) {
+            
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
+            stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: { (sampleBuffer, error) in
+                
+                if sampleBuffer != nil {
+                    var imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    var dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    var cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true, .RenderingIntentDefault)
+                    
+                    var image = UIImage(CGImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.Right)
+                    
+                    self.tempImageView.image = image
+                    self.tempImageView.hidden = false
+                }
+            })
+        }
+    }
+    
+    var didTakePhoto = Bool()
+    
+    func didPressTakeAnother() {
         
-        
-        picker.sourceType = .Camera
-        
-        presentViewController(picker, animated: true, completion: nil)
+        if didTakePhoto == true {
+            
+            tempImageView.hidden = true
+            didTakePhoto = false
+            
+        } else {
+            
+            captureSession?.startRunning()
+            didTakePhoto = true
+            didPressTakePhoto()
+        }
         
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        ImageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
+    @IBAction func retakeAction(sender: UIButton) {
+        didPressTakeAnother()
     }
-
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        
+//    }
 }
