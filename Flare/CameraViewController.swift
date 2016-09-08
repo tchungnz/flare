d1397050f08ca519483799197f69738845591e7f
Mali Michael <malimccalla@gmail.com>
2016-09-08 14:46:53 +0100

    camera

---
+++ Flare/CameraViewController.swift
@@ -0,0 +1,48 @@
+//
+//  CameraViewController.swift
+//  Flare
+//
+//  Created by Mali McCalla on 08/09/2016.
+//  Copyright © 2016 appflare. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    
+    @IBOutlet weak var Camera: UIButton!
+    @IBOutlet weak var ImageDisplay: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    @IBAction func CameraAction(sender: UIButton) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        
+        
+        picker.sourceType = .Camera
+        
+        presentViewController(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        ImageDisplay.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
