91f5b6fef53ea3d88bfd8bbf1d4f7dc4b6cfe9fe
tchungnz <tchung.nz@gmail.com>
2016-09-09 18:35:26 +0100

    merged username and connectform

--- Flare/CameraViewController.swift
+++ Flare/CameraViewController.swift
@@ -167,11 +167,33 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     
     func swipeUp(recognizer : UISwipeGestureRecognizer) {
+        
+        // MARK: Upload image to storage
+        var data = NSData()
+        data = UIImageJPEGRepresentation(self.tempImageView.image!, 0.8)!
+        
+        let storage = FIRStorage.storage()
+        let storageRef = storage.referenceForURL("gs://flare-1ef4b.appspot.com")
+        
+        let imageRef = storageRef.child("images/flare\(NSUUID().UUIDString).jpg")
+        
+        let uploadTask = imageRef.putData(data, metadata: nil) { metadata, error in
+            if (error != nil) {
+                puts("Error")
+            }
+            else {
+                let downloadURL = metadata!.downloadURL
+            }
+        }
+        
+        // MARK: Save flare data to database
+        
         var ref = FIRDatabase.database().reference()
         let flareRef = ref.childByAppendingPath("flares")
         let timestamp = FIRServerValue.timestamp()
         let user = FIRAuth.auth()?.currentUser
-        let flare1 = ["title": self.flareTitle.text!, "subtitle": user!.email! as String, "latitude": self.flareLatitude! as String, "longitude": self.flareLongitude! as String, "timestamp": timestamp]
+        print(user!.email)
+        let flare1 = ["title": self.flareTitle.text!, "subtitle": "test@test.com", "latitude": self.flareLatitude! as String, "longitude": self.flareLongitude! as String, "timestamp": timestamp]
         let flare1Ref = flareRef.childByAutoId()
         flare1Ref.setValue(flare1)
 
