0aff93b562ea08ff7e4f001d6477d20bfb278932
tchungnz <tchung.nz@gmail.com>
2016-09-10 18:52:33 +0100

    added new detailed view

---
+++ Flare/FlareDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FlareDetailViewController.swift
+//  Flare
+//
+//  Created by Tim Chung on 10/09/2016.
+//  Copyright © 2016 appflare. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class FlareDetailViewController: UIViewController {
+    
+//    @IBOutlet weak var flareTitleLabel: UILabel!
+    var testVariable: String?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("*************")
+        print(testVariable)
+//        flareTitleLabel =
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
--- Flare/VCMapView.swift
+++ Flare/VCMapView.swift
@@ -33,16 +33,37 @@ extension MapViewController {
         let btn = UIButton(type: .DetailDisclosure)
         annotationView!.rightCalloutAccessoryView = btn
         
-        
         return annotationView
     }
     
     
-//    func mapView(mapView: MKMapView!, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-//        
-//        if control == annotationView.rightCalloutAccessoryView {
-//            performSegueWithIdentifier("FlareDetail", sender: self)
+    func mapView(mapView: MKMapView!, annotationView: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        
+        if control == annotationView.rightCalloutAccessoryView {
+            performSegueWithIdentifier("flareDetail", sender: annotationView)
+        }
+    }
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if (segue.identifier == "flareDetail") {
+//            if let waypoint = (sender as? MKAnnotationView)?.annotation as? MKAnnotation {
+//                if let ivc = segue.destinationViewController as? FlareDetailViewController {
+//                    ivc.title = waypoint.title
+//                    ivc.subtitle = waypoint.subtitle
+//                }
+//            }
 //        }
 //    }
-
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "fareDetail") {
+        
+        // Create a variable that you want to send
+        var newTestVariable = "some text"
+        
+        // Create a new variable to store the instance of PlayerTableViewController
+        let destinationVC = segue.destinationViewController as! FlareDetailViewController
+        destinationVC.testVariable = newTestVariable
+        }
+}
 }
