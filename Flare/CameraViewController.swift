20ae840696cc9f8fd53e815b6bffaf0ec02cefa8
JAstbury <jess.astbury@hotmail.com>
2016-09-10 15:08:44 +0100

    Move swipe logic into separate file

--- Flare/CameraViewController.swift
+++ Flare/CameraViewController.swift
@@ -13,7 +13,7 @@ import Firebase
 import FirebaseDatabase
 import CoreLocation
 
-class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, CLLocationManagerDelegate, UITextFieldDelegate {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCaptureStillImageOutput?
@@ -194,61 +194,11 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
             
             dispatch_after(dispatchTime, dispatch_get_main_queue(), {
                 
-                // MARK: Upload image to storage
-                var data = NSData()
-                data = UIImageJPEGRepresentation(self.tempImageView.image!, 0.8)!
-                
-                let storage = FIRStorage.storage()
-                let storageRef = storage.referenceForURL("gs://flare-1ef4b.appspot.com")
-                
-                let imageString = NSUUID().UUIDString
-                let imageRef = storageRef.child("images/flare\(imageString).jpg")
-                
-                let uploadTask = imageRef.putData(data, metadata: nil) { metadata, error in
-                    if (error != nil) {
-                        puts("Error")
-                    }
-                    else {
-                        let downloadURL = metadata!.downloadURL
-                    }
-                }
-                
-                
-                // MARK: How to get image out of storage
-//
-//                let islandRef = storageRef.child("images/flare\(imageString).jpg")
-//                // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-//                islandRef.dataWithMaxSize(1 * 1024 * 1024) { (data, error) -> Void in
-//                    if (error != nil) {
-//                        print("Error!")
-//                    } else {
-//                        let islandImage: UIImage! = UIImage(data: data!)
-//                    }
-//                }
-                
-                // MARK: Save flare data to database
-                
-                
-                var ref = FIRDatabase.database().reference()
-                let flareRef = ref.childByAppendingPath("flares")
-                let timestamp = FIRServerValue.timestamp()
-                let user = FIRAuth.auth()?.currentUser
-                print(user!.email)
-                // Put a guard on the email code below:
-                let flare1 = ["title": self.flareTitle.text!, "subtitle": user!.email! as String, "imageRef": "images/flare\(imageString).jpg", "latitude": self.flareLatitude! as String, "longitude": self.flareLongitude! as String, "timestamp": timestamp]
-                let flare1Ref = flareRef.childByAutoId()
-                flare1Ref.setValue(flare1)
+                self.onSwipe()
                 
                 self.performSegueWithIdentifier("returnMap", sender: self)
             })
         }
-
-    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last
-        self.flareLatitude = String(location!.coordinate.latitude)
-        self.flareLongitude = String(location!.coordinate.longitude)
-        
-    }
     
     
 //    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
--- Flare/SwipeViewController.swift
+++ Flare/SwipeViewController.swift
@@ -12,8 +12,61 @@ import Firebase
 import FirebaseDatabase
 import CoreLocation
 
-extension MapViewController {
-
-
-
+extension CameraViewController: CLLocationManagerDelegate {
+    
+    func onSwipe() {
+        
+        // MARK: Upload image to storage
+        
+        var data = NSData()
+        data = UIImageJPEGRepresentation(self.tempImageView.image!, 0.8)!
+        
+        let storage = FIRStorage.storage()
+        let storageRef = storage.referenceForURL("gs://flare-1ef4b.appspot.com")
+        
+        let imageString = NSUUID().UUIDString
+        let imageRef = storageRef.child("images/flare\(imageString).jpg")
+        
+        let uploadTask = imageRef.putData(data, metadata: nil) { metadata, error in
+            if (error != nil) {
+                puts("Error")
+            }
+            else {
+                let downloadURL = metadata!.downloadURL
+            }
+        }
+        
+        // MARK: Save flare data to database
+        
+        
+        var ref = FIRDatabase.database().reference()
+        let flareRef = ref.childByAppendingPath("flares")
+        let timestamp = FIRServerValue.timestamp()
+        let user = FIRAuth.auth()?.currentUser
+        print(user!.email)
+        // Put a guard on the email code below:
+        let flare1 = ["title": self.flareTitle.text!, "subtitle": user!.email! as String, "imageRef": "images/flare\(imageString).jpg", "latitude": self.flareLatitude! as String, "longitude": self.flareLongitude! as String, "timestamp": timestamp]
+        let flare1Ref = flareRef.childByAutoId()
+        flare1Ref.setValue(flare1)
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        self.flareLatitude = String(location!.coordinate.latitude)
+        self.flareLongitude = String(location!.coordinate.longitude)
+    }
+    
+    
+    // MARK: How to get image out of storage
+    //
+    //                let islandRef = storageRef.child("images/flare\(imageString).jpg")
+    //                // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
+    //                islandRef.dataWithMaxSize(1 * 1024 * 1024) { (data, error) -> Void in
+    //                    if (error != nil) {
+    //                        print("Error!")
+    //                    } else {
+    //                        let islandImage: UIImage! = UIImage(data: data!)
+    //                    }
+    //                }
+    
 }
