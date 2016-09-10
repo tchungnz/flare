b75be8dd6c5c7c73c6c0aa66b5dcf9573d4d17e4
Tommy Williams <tom@teow.co.uk>
2016-09-10 23:39:31 +0100

    slightly weird vertically scrolling images in flare detail view

--- Flare/FlareDetailViewController.swift
+++ Flare/FlareDetailViewController.swift
@@ -17,6 +17,7 @@ class FlareDetailViewController: UIViewController {
     @IBOutlet weak var flareTitleLabel: UILabel!
     @IBOutlet weak var flareSubtitleLabel: UILabel!
     @IBOutlet weak var flareImage: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var flareExport: Flare?
 
@@ -24,30 +25,26 @@ class FlareDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.scrollView.contentSize = CGSize(width:1080, height: 1920)
         
         // MARK: How to get image out of storage
         let storage = FIRStorage.storage()
         let storageRef = storage.referenceForURL("gs://flare-1ef4b.appspot.com")
-        let islandRef = storageRef.child("images/flare\(flareExport!.imageRef!).jpg")
+        let islandRef = storageRef.child("\(flareExport!.imageRef!)")
         // Download in memory with a maximum allowed size of 1MB (1 * 1024 * 1024 bytes)
-            islandRef.dataWithMaxSize(1 * 1024 * 1024) { (data, error) -> Void in
+            islandRef.dataWithMaxSize(1 * 1920 * 1080) { (data, error) -> Void in
                 if (error != nil) {
                     print("Error!")
                 } else {
                     let islandImage: UIImage! = UIImage(data: data!)
-                    let flareImage = UIImageView(image: islandImage!)
-                    flareImage.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
-                    print(flareImage)
-                    self.view.addSubview(flareImage)
+                    self.flareImage.image = islandImage
+                    print(islandImage.size)
                     }
                 }
         
+                flareTitleLabel.text = flareExport!.title!
+                flareSubtitleLabel.text = flareExport!.subtitle!
         
-        
-//        let printTitle = aTitle
-        print("*************IMAGEREF**************")
-        print(flareExport!.imageRef)
-
     }
 
     override func didReceiveMemoryWarning() {
