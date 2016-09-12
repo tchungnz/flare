7d1a619a0b96df8e43509ab836971c9ab451c391
Mali Michael <malimccalla@gmail.com>
2016-09-12 17:50:35 +0100

    closer map view

--- Flare/MapDisplayController.swift
+++ Flare/MapDisplayController.swift
@@ -23,7 +23,7 @@ extension MapViewController {
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location = locations.last
         let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
         self.mapView.setRegion(region, animated: true)
         self.locationManager.stopUpdatingLocation()
     }
