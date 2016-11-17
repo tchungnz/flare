d4ea2a5290591c3fd6a2004e8d3ad478a81f7b80
Mali Michael <malimccalla@gmail.com>
2016-11-17 16:14:21 +0000

    adds onboarding view controller

---
+++ Flare/OnBoardingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  OnBoardingViewController.swift
+//  Flare
+//
+//  Created by Mali McCalla on 17/11/2016.
+//  Copyright © 2016 appflare. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
